ff10b72114170cc01bcd344e992a5facb8e97a6e
alexbozhkov <alexander.bojkov89@gmail.com>
2024-03-21 17:29:58 +0200

    Create two container based services with the ability of http communication between them

---
+++ service_one/app/main.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+import os
+
+import asyncio
+from typing import Final
+
+from aiohttp import ClientSession
+from fastapi import Depends, FastAPI
+from starlette.requests import Request
+
+app: Final = FastAPI()
+SERVICE_TWO_URL = os.getenv("SERVICE_TWO_URL")
+SERVICE_TWO_PORT = os.getenv("SERVICE_TWO_PORT")
+
+
+@app.on_event("startup")
+async def startup_event():
+    setattr(app.state, "client_session", ClientSession(raise_for_status=True))
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await asyncio.wait((app.state.client_session.close()), timeout=5.0)
+
+
+def client_session_dep(request: Request) -> ClientSession:
+    return request.app.state.client_session
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "FROM SERVICE ONE"}
+
+
+@app.get("/test")
+async def root(
+    client_session: ClientSession = Depends(client_session_dep),
+) -> str:
+    async with client_session.get(f"http://{SERVICE_TWO_URL}:{SERVICE_TWO_PORT}/") as the_response:
+        return await the_response.text()
